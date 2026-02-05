adcc6e281f22e783a455084a479e1149d91004e2
Vd_Alexey_Vl <alexey28102@gmail.com>
2026-02-05 22:38:42 +0300

    Еще раз исправил на обратную сортировку.

--- DZ_5_4.cpp
+++ DZ_5_4.cpp
@@ -17,7 +17,7 @@ int main()
 
         for (int i = 0; i < n - 1; i++)
         {
-            for (int j = 0; j < n - i - 1; j++)
+            for (int j = n - i - 2; j >= 0; j--)
             {
                 if (mass[j] < mass[j + 1])
                 {
